6367ee181890c4308561d78ad9c3ed00173a2216
Nazorek <nazorekpierre@gmail.com>
2025-04-02 10:31:02 -0300

    Update teste do .C

--- teste do .C
+++ teste do .C
@@ -12,7 +12,7 @@ float le = ((hora*3600)+(min*60)+seg);
 printf("quantidade de segundos %\n", le);
 }
 else{
-float  gal =((hora*00)+min+(seg(60)));
+float  gal =((hora*60)+min+(seg/60);
 printf("quantidade de minutos %.5f\n", gal);
 }
 
