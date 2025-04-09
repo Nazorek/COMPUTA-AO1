41b9f3e7b31a75700808b1b344ef7936e5c4bc11
Nazorek <nazorekpierre@gmail.com>
2025-04-09 07:45:34 -0300

    media.c

---
+++ media.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int quantidade;
+    float nota, soma = 0, media;
+
+    printf("===== CALCULO DE MEDIA =====\n");
+
+    printf("Informe a quantidade de notas: ");
+    scanf("%d", &quantidade);
+
+    for(int i = 1; i <= quantidade; i++) {
+        printf("Digite a nota %d: ", i);
+        scanf("%f", &nota);
+        soma += nota;
+    }
+
+    media = soma / quantidade;
+
+    printf("Media final: %.2f\n", media);
+
+    if(media >= 6) {
+        printf("Situacao: Aprovado\n");
+    } else if(media >= 5) {
+        printf("Situacao: Recuperacao\n");
+    } else {
+        printf("Situacao: Reprovado\n");
+    }
+
+    return 0;
+}
